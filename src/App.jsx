0e778373bb5e97099bb2fd42aff991c3832bd5d9
Lucas Alcocer <lucasalcocer174@gmail.com>
2024-08-08 19:36:01 -0300

    agregados los archivos Cursos.jsx y Registros.jsx en el proyecto.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import {Redirect, Route} from "wouter";
 import Login from "./componentes/Login";
 import Alumnos from "./componentes/Alumnos";
+import Registros from "./componentes/Registros";
+import Cursos from "./componentes/Cursos";
 import './App.css'
 
 export default class App extends Component {
@@ -28,11 +30,27 @@ export default class App extends Component {
           <Alumnos 
             cambiarMenu= {(opcion) => this.setState({menu:opcion})}
           />
-          </Route>
+        </Route>
+        <Route path="/registros">
+          <Registros 
+            cambiarMenu={(opcion) => this.setState({ menu: opcion })} 
+          />
+        </Route>
+        <Route path="/cursos">
+          <Cursos 
+            cambiarMenu={(opcion) => this.setState({ menu: opcion })} 
+          />
+        </Route>
         {/*footer */}
       </>
     )
   }
 }
+
+
+
+
+        
+
 
 //si tenias el token valido
